40f5c96acbb8bccd3c9bc85fe84ebea66a8340d8
Ivan Shumeiko <ivan.shumeiko24@gmail.com>
2025-06-10 15:28:25 +0300

    feat: create booking-service structure

---
+++ src/main/java/intexsoft/practice/booking_service/BookingServiceApplication.java
@@ -0,0 +1,13 @@
+package intexsoft.practice.booking_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingServiceApplication.class, args);
+	}
+
+}
