dcbfcafab763286d6fb0f9efa4d4a37ff12b00c0
coldsteeze <nikitaproduction@mail.ru>
2025-06-12 11:26:10 +0300

    feat(kafka-producer-starter): initial project structure with pom and source folders

---
+++ kafka-producer-lib/src/test/java/intexsoft/practice/kafka_producer_lib/KafkaProducerLibApplicationTests.java
@@ -0,0 +1,13 @@
+package intexsoft.practice.kafka_producer_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
