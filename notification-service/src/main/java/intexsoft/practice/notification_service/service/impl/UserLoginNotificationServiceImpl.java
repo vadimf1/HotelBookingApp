ba80ab6da5f471e269c77712c53cd82fbefefa3e
coldsteeze <nikitaproduction@mail.ru>
2025-06-12 13:14:39 +0300

    feat: add MailContentBuilder for generating email content using FreeMarker

--- notification-service/src/main/java/intexsoft/practice/notification_service/service/impl/UserLoginNotificationServiceImpl.java
+++ notification-service/src/main/java/intexsoft/practice/notification_service/service/impl/UserLoginNotificationServiceImpl.java
@@ -19,27 +19,23 @@ public class UserLoginNotificationServiceImpl implements NotificationService<Use
             1L, "user1@example.com",
             2L, "user2@example.com"
     );
+
+    private static final String LOGIN_TEMPLATE = "login-notification.ftl";
     private final MailSenderService mailSenderService;
+    private final FreeMarkerMailContentBuilder contentBuilder;
 
     @Override
     public void notify(UserLoginNotificationDto dto) {
         String email = USER_EMAILS.getOrDefault(dto.userId(), "default@example.com");
 
-        String subject = "Новая авторизация в системе";
-        String body = String.format("""
-                        Привет!
-                        Кто-то вошёл в ваш аккаунт.
-
-                        IP: %s
-                        Страна: %s
-                        Браузер: %s
-                        Время: %s
-                        """,
-                dto.ip(),
-                dto.country(),
-                dto.userAgent(),
-                dto.timestamp()
+        String subject = "Новый вход в аккаунт";
+        Map<String, Object> model = Map.of(
+                "ip", dto.ip(),
+                "country", dto.country(),
+                "userAgent", dto.userAgent(),
+                "timestamp", dto.timestamp()
         );
+        String body = contentBuilder.build(LOGIN_TEMPLATE, model);
 
         mailSenderService.sendEmail(email, subject, body);
         log.info("Уведомление отправлено на почту {}", email);
