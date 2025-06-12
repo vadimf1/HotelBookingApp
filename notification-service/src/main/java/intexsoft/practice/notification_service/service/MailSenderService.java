d2cdf544bf95fe24e44e5069ea5f5d89d4dafb1f
coldsteeze <nikitaproduction@mail.ru>
2025-06-12 13:26:02 +0300

    refactor: rename variable 'to' to 'recipientEmail' for clarity

--- notification-service/src/main/java/intexsoft/practice/notification_service/service/MailSenderService.java
+++ notification-service/src/main/java/intexsoft/practice/notification_service/service/MailSenderService.java
@@ -1,5 +1,5 @@
 package intexsoft.practice.notification_service.service;
 
 public interface MailSenderService {
-    void sendEmail(String to, String subject, String body);
+    void sendEmail(String recipientEmail, String subject, String body);
 }
