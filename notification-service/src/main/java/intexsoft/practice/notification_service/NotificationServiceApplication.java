67dc7e3e7687dcaae2bc84fb98095628138d3048
coldsteeze <nikitaproduction@mail.ru>
2025-06-12 19:27:34 +0300

    feat: add test controller for Kafka producer lib

--- notification-service/src/main/java/intexsoft/practice/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/intexsoft/practice/notification_service/NotificationServiceApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+		"intexsoft.practice.notification_service",
+		"intexsoft.practice.kafka_producer_lib" // <--- добавить это
+})
 @EnableConfigurationProperties(MailProperties.class)
 public class NotificationServiceApplication {
 
