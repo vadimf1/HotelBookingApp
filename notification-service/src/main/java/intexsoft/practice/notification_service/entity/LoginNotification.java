e35ec443f22fce9320c83aab33121eab807f83df
coldsteeze <nikitaproduction@mail.ru>
2025-06-17 12:18:08 +0300

    fix: switch @generatedvalue to @uuidgenerator

--- notification-service/src/main/java/intexsoft/practice/notification_service/entity/LoginNotification.java
+++ notification-service/src/main/java/intexsoft/practice/notification_service/entity/LoginNotification.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -19,7 +20,7 @@ import java.util.UUID;
 public class LoginNotification {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @UuidGenerator
     @Column(name = "id", nullable = false, updatable = false)
     private UUID id;
 
