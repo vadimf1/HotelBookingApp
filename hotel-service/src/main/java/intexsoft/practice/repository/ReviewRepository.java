df1f79121a1d3144f202eff8065394c966459123
vadimf1 <vadimfunt1@gmail.com>
2025-06-23 22:12:28 +0300

    feat: add github actions delete: review

--- hotel-service/src/main/java/intexsoft/practice/repository/ReviewRepository.java
+++
@@ -1,9 +0,0 @@
-package intexsoft.practice.repository;
-
-import intexsoft.practice.model.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface ReviewRepository extends JpaRepository<Review, UUID> {
-}
