b55c19f88906c7e1e7b1f72c54cb7fed30eab22c
coldsteeze <nikitaproduction@mail.ru>
2025-06-23 13:56:48 +0300

    feat: add retry mechanism for clients with custom exceptions

--- notification-service/src/main/java/intexsoft/practice/notification_service/NotificationServiceApplication.java
+++ notification-service/src/main/java/intexsoft/practice/notification_service/NotificationServiceApplication.java
@@ -3,9 +3,11 @@ package intexsoft.practice.notification_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication()
+@EnableRetry
 public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
---
+++ notification-service/src/main/java/intexsoft/practice/notification_service/exception/userClient/UserServiceException.java
@@ -0,0 +1,7 @@
+package intexsoft.practice.notification_service.exception.userClient;
+
+public class UserServiceException extends RuntimeException {
+    public UserServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
