2f7b7c66418d39c3af882e6f04e86c79b47f7a24
Ivan_Samusevich <samusevich.ivan@list.ru>
2025-06-24 15:07:30 +0300

    add liquibase

--- src/main/java/IntexSoft/Authorization/models/User.java
+++ src/main/java/IntexSoft/Authorization/models/User.java
@@ -3,26 +3,18 @@ package IntexSoft.Authorization.models;
 import jakarta.persistence.*;
 import lombok.*;
 
-
 @Data
 @Entity
 @Table(name = "users")
-public class User{
+public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userId;
 
-    @Column(name = "first_name", nullable = false, length = 50)
     private String firstName;
-
-    @Column(name = "last_name", nullable = false, length = 50)
     private String lastName;
-
-    @Column(name = "email", nullable = false, length = 100, unique = true)
     private String email;
-
-    @Column(name = "password", nullable = false, length = 255)
     private String password;
 
 }
