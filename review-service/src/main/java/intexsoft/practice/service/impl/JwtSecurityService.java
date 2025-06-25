34466ded5a900e89576dc3225c94f2e6eb2f7624
vadimf1 <vadimfunt1@gmail.com>
2025-06-25 13:36:11 +0300

    update: secret key retrieval

--- review-service/src/main/java/intexsoft/practice/service/impl/JwtSecurityService.java
+++ review-service/src/main/java/intexsoft/practice/service/impl/JwtSecurityService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -11,7 +12,9 @@ import java.util.Date;
 
 @Service
 public class JwtSecurityService {
-    private final String SECRET_KEY = System.getProperty("jwt.secret");
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
 
     private SecretKey getSigningKey() {
         byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
